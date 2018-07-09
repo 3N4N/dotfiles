89de41597798e03e74289462caa3bcca638c9486
Enan Ajmain <3nan.ajmain@gmail.com>
2018-07-09 21:09:26 +0600

    Add backgrounds

--- install.sh
+++ install.sh
@@ -7,36 +7,38 @@ I3DIR=$HOME/.config/i3
 RANGERDIR=$HOME/.config/ranger
 CMUSDIR=$HOME/.cmus
 FONTDIR=$HOME/.fonts
+BACKGROUNDDIR=$HOME/.local/share/backgrounds
 
 mkdir -p $VIMDIR
-rm $VIMDIR/init.vim
+rm -f $VIMDIR/init.vim
 ln -s $DIR/init.vim $VIMDIR
 
 mkdir -p $EMACSDIR
-rm $EMACSDIR/init.el
+rm -f $EMACSDIR/init.el
 ln -s $DIR/init.el $EMACSDIR
 
 mkdir -p $I3DIR
-rm $I3DIR/config $I3DIR/i3blocks.conf
+rm -f $I3DIR/config $I3DIR/i3blocks.conf
 rm -rf $I3DIR/scripts
 ln -s $DIR/config $I3DIR
 ln -s $DIR/i3blocks.conf $I3DIR
 ln -s $DIR/scripts $I3DIR
 
-rm $HOME/.config/compton.conf
+rm -f $HOME/.config/compton.conf
 ln -s $DIR/compton.conf $HOME/.config
+
+rm -f $HOME/.zshrc $HOME/.bashrc $HOME/.tmux.conf $HOME/.gitconfig
+ln -s $DIR/.zshrc $DIR/.bashrc $DIR/.tmux.conf $DIR/.gitconfig $HOME/
+
+mkdir -p $CMUSDIR
+rm -f $CMUSDIR/cmus.theme
+ln -s $DIR/cmus.theme $CMUSDIR/cmus.theme
 
 rm -rf $HOME/.config/ranger
 ln -s $DIR/ranger $RANGERDIR
 
-rm -rf $HOME/.zshrc $HOME/.bashrc $HOME/.tmux.conf $HOME/.gitconfig
-ln -s $DIR/.zshrc $DIR/.bashrc $DIR/.tmux.conf $DIR/.gitconfig $HOME/
+rm -rf $FONTDIR
+ln -s $DIR/fonts $FONTDIR
 
-mkdir -p $CMUSDIR
-rm -rf $CMUSDIR/cmus.theme
-ln -s $DIR/cmus.theme $CMUSDIR/cmus.theme
-
-mkdir -p $FONTDIR
-rm $FONTDIR/* -rf
-cp fonts/*.ttf fonts/*.otf $HOME/.fonts
-fc-cache -f
+rm -rf $BACKGROUNDDIR
+ln -s $DIR/backgrounds $BACKGROUNDDIR
