2c54a2d8e509e59b4f4ce517aaab480bfd9a9b50
Enan Ajmain <3nan.ajmain@gmail.com>
2021-06-15 10:07:50 +0600

    Use uncrustify to dotfiles for code-formatting

--- install.sh
+++ install.sh
@@ -3,42 +3,43 @@
 DIR=$HOME/projects/dotFiles
 
 DOTFILES=(
-	"bin"
-	".bashrc"
-	".bash_profile"
-	".gitconfig"
-	".profile"
-	".tmux.conf"
-	".xinitrc"
-	".config/mpv"
-	".config/nvim"
-	".config/ranger"
-	".local/share/fonts"
+    "bin"
+    ".bashrc"
+    ".bash_profile"
+    ".gitconfig"
+    ".profile"
+    ".tmux.conf"
+    ".uncrustify.cfg"
+    ".xinitrc"
+    ".config/mpv"
+    ".config/nvim"
+    ".config/ranger"
+    ".local/share/fonts"
 )
 
 for dotfile in "${DOTFILES[@]}";do
-	rm -rf "${HOME}/${dotfile}"
-	ln -sf "${DIR}/${dotfile}" "${HOME}/${dotfile}"
+    rm -rf "${HOME}/${dotfile}"
+    ln -sf "${DIR}/${dotfile}" "${HOME}/${dotfile}"
 done
 
 if [ "$1" == "-a" ]; then
-	sudo apt install \
-		acpi \
-		curl \
-		deluge \
-		ffmpeg \
-		git \
-		htop \
-		mpv \
-		mupdf \
-		pavucontrol \
-		rar \
-		silversearcher-ag \
-		tree \
-		uncrustify \
-		wget \
-		x11-xkb-utils \
-		xbacklight \
-		xclip \
-		zip
+    sudo apt install \
+        acpi \
+        curl \
+        deluge \
+        ffmpeg \
+        git \
+        htop \
+        mpv \
+        mupdf \
+        pavucontrol \
+        rar \
+        silversearcher-ag \
+        tree \
+        uncrustify \
+        wget \
+        x11-xkb-utils \
+        xbacklight \
+        xclip \
+        zip
 fi
