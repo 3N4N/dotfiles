0fdcfcccee4e8c6c2cc9d2449abd412d03b7b785
Enan Ajmain <3nan.ajmain@gmail.com>
2021-10-06 17:39:08 +0600

    Add screenshot folder for mpv and stop littering

--- install.ps1
+++ install.ps1
@@ -1,11 +1,16 @@
 $CURDIR=(Get-Location | Foreach-Object { $_.Path })
 
 $VIMDIR="$HOME/AppData/Local/nvim"
+$MPVDIR="$HOME/AppData/Roaming/mpv"
+
+if (-Not(Test-Path -Path $PROFILE)) {
+    New-Item -Type SymbolicLink -Path $PROFILE -Value $CURDIR/Microsoft.PowerShell_profile.ps1
+}
 
 if (-Not(Test-Path -Path "$VIMDIR")) {
     New-Item -Type Junction -Path "$VIMDIR" -Value $CURDIR/.config/nvim/
 }
 
-if (-Not(Test-Path -Path $PROFILE)) {
-    New-Item -Type SymbolicLink -Path $PROFILE -Value $CURDIR/Microsoft.PowerShell_profile.ps1
+if (-Not(Test-Path -Path "$MPVDIR")) {
+    New-Item -Type Junction -Path "$MPVDIR" -Value $CURDIR/.config/mpv/
 }
