f3f5192b49b333d868911a384c3105a451fbd97d
Enan Ajmain <3nan.ajmain@gmail.com>
2021-11-10 13:25:28 +0600

    CMD: use 7z to extract all archives

--- bin/extract.cmd
+++ bin/extract.cmd
@@ -1,42 +1,33 @@
 @echo off
 
-:: if not exist %1 (
-::     echo %1 doesn't exist
-::     exit /b 1
-:: )
-
 set filename=%1
 
 if not x%filename%==x%filename:.tar.bz2=% (
-    tar xvjf "%filename%"
+    7z x "%filename%" -so | 7z x -aoa -si -ttar
 ) else if not x%filename%==x%filename:.tar.xz=% (
-    7z x "%filename%"
-    set tarfile=%filename:.tar.xz=.tar%
-    tar xvf "%tarfile%"
+    7z x "%filename%" -so | 7z x -aoa -si -ttar
 ) else if not x%filename%==x%filename:.tar.gz=% (
-    tar xvzf "%filename%"
+    7z x "%filename%" -so | 7z x -aoa -si -ttar
 ) else if not x%filename%==x%filename:.bz2=% (
-    bunzip2 "%filename%"
+    7z x "%filename%"
 ) else if not x%filename%==x%filename:.rar=% (
-    rar x "%filename%"
+    7z x "%filename%"
 ) else if not x%filename%==x%filename:.gz=% (
-    gunzip "%filename%"
+    7z x "%filename%"
 ) else if not x%filename%==x%filename:.tar=% (
-    tar xvf "%filename%"
+    7z x "%filename%"
 ) else if not x%filename%==x%filename:.tbz2=% (
-    tar xvjf "%filename%"
+    7z x "%filename%"
 ) else if not x%filename%==x%filename:.tgz=% (
-    tar xvzf "%filename%"
+    7z x "%filename%"
 ) else if not x%filename%==x%filename:.zip=% (
-    unzip "%filename%"
+    7z x "%filename%"
 ) else if not x%filename%==x%filename:.Z=% (
-    uncompress "%filename%"
+    7z x "%filename%"
 ) else if not x%filename%==x%filename:.xz=% (
-    xz -d "%filename%"
+    7z x "%filename%"
 ) else if not x%filename%==x%filename:.7z=% (
     7z x "%filename%"
-) else if not x%filename%==x%filename:.a=% (
-    ar x "%filename%"
 ) else (
     echo "No uncompressing program found for this filetype."
 )
