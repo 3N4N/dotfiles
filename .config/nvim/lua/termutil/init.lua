be12fa3405c190834e96111dcc26f58c8353c250
Enan Ajmain <3nan.ajmain@gmail.com>
2021-12-13 22:29:52 +0600

    VIM: send extra newline for ft-python in termutil

--- .config/nvim/lua/termutil/init.lua
+++ .config/nvim/lua/termutil/init.lua
@@ -5,6 +5,7 @@ M.sendToTerm = function(visual)
     assert(vim.g.termutilchan, "g:termutilchan not set")
 
     local termutilchan = vim.g.termutilchan
+
     if visual == 0 then
         local line = vim.api.nvim_get_current_line()
         vim.api.nvim_chan_send(termutilchan, line..'\r')
@@ -12,6 +13,9 @@ M.sendToTerm = function(visual)
         local lines = require("luautil/selection").get_selection()
         for _, line in pairs(lines) do
             vim.api.nvim_chan_send(termutilchan, line..'\r')
+        end
+        if vim.bo.filetype == "python" then
+            vim.api.nvim_chan_send(termutilchan, '\r')
         end
     end
 end
