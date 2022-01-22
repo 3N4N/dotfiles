e331b584c169696bb0cb15b5c62399b95290375c
Enan Ajmain <3nan.ajmain@gmail.com>
2022-01-22 23:36:51 +0600

    VIM: fix clipboard in WSL

--- .config/nvim/lua/user/clipboard.lua
+++ .config/nvim/lua/user/clipboard.lua
@@ -11,6 +11,19 @@ if vim.g.env == "UNIX" then
         },
         cache_enabled = 1,
     }
+elseif vim.g.env == "WSL" then
+    vim.g.clipboard = {
+        name = "win32yank_nvim",
+        copy = {
+            ["+"] = "win32yank.exe -i",
+            ["*"] = "win32yank.exe -i",
+        },
+        paste = {
+            ["+"] = "win32yank.exe -o",
+            ["*"] = "win32yank.exe -o",
+        },
+        cache_enabled = 1,
+    }
 end
 
 vim.opt.clipboard:append("unnamed")
