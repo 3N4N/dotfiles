b43cfeefff11348950442b53484ae08385c2f00c
Enan Ajmain <3nan.ajmain@gmail.com>
2022-01-25 00:54:42 +0600

    VIM: send \n instead of \r to IPython
    IPython doesn't take \n from lua script, but takes it if input as lua
    command from cmdline. Who knows why.
    
    I don't know if it'll stop working in Linux. If it does then I'll have
    to use a variable to set \n for python files and \r otherwise.

--- .config/nvim/lua/termutil/init.lua
+++ .config/nvim/lua/termutil/init.lua
@@ -8,16 +8,16 @@ M.sendToTerm = function(visual)
 
     if visual == 0 then
         local line = vim.api.nvim_get_current_line()
-        vim.api.nvim_chan_send(termutilchan, line..'\r')
+        vim.api.nvim_chan_send(termutilchan, line..'\n')
     else
         local lines = require("util.selection").get_selection()
         for _, line in pairs(lines) do
             if line ~= "" then
-                vim.api.nvim_chan_send(termutilchan, line..'\r')
+                vim.api.nvim_chan_send(termutilchan, line..'\n')
             end
         end
         if vim.bo.filetype == "python" then
-            vim.api.nvim_chan_send(termutilchan, '\r')
+            vim.api.nvim_chan_send(termutilchan, '\n')
         end
     end
 end
