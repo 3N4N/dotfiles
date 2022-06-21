0bf8cca26e37071d47554222d590e4ccc23163f3
Enan Ajmain <3nan.ajmain@gmail.com>
2022-06-21 10:37:19 +0600

    VIM: pwsh settings (shellpipe) update

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -33,7 +33,7 @@ end
 -- vim.opt.shell = 'pwsh.exe'
 -- vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
 -- vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
--- vim.opt.shellpipe = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+-- vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
 -- vim.opt.shellquote = ""
 -- vim.opt.shellxquote = ""
 -- vim.opt.ssl = false
