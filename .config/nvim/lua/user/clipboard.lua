8316200223c4d34a9e1ed6639ba7bd35d78313c8
Enan Ajmain <3nan.ajmain@gmail.com>
2022-07-08 23:32:40 +0600

    vim: fix win32yank pasting in wsl
    Problem: pasted ^M chars when pasting from Win clipboard
    Solution: let win32yank know what line ending it's getting

--- .config/nvim/lua/user/clipboard.lua
+++ .config/nvim/lua/user/clipboard.lua
@@ -15,12 +15,12 @@ elseif vim.g.env == "WSL" then
     vim.g.clipboard = {
         name = "win32yank_nvim",
         copy = {
-            ["+"] = "win32yank.exe -i",
-            ["*"] = "win32yank.exe -i",
+            ["+"] = "win32yank.exe -i --crlf",
+            ["*"] = "win32yank.exe -i --crlf",
         },
         paste = {
-            ["+"] = "win32yank.exe -o",
-            ["*"] = "win32yank.exe -o",
+            ["+"] = "win32yank.exe -o --lf",
+            ["*"] = "win32yank.exe -o --lf",
         },
         cache_enabled = 1,
     }
