24c2be14e22e381e34a1742ba42dfe2b3798668a
Enan Ajmain <3nan.ajmain@gmail.com>
2022-07-11 14:28:07 +0600

    vim: remove unused lsp servers

--- .config/nvim/lua/user/lspconfig.lua
+++ .config/nvim/lua/user/lspconfig.lua
@@ -37,23 +37,6 @@ local lsp_flags = {
   debounce_text_changes = 150,
 }
 
--- require('lspconfig')['pyright'].setup{
---     on_attach = on_attach,
---     flags = lsp_flags,
--- }
--- require('lspconfig')['tsserver'].setup{
---     on_attach = on_attach,
---     flags = lsp_flags,
--- }
--- require('lspconfig')['rust_analyzer'].setup{
---     on_attach = on_attach,
---     flags = lsp_flags,
---     -- Server-specific settings...
---     settings = {
---       ["rust-analyzer"] = {}
---     }
--- }
-
 require'lspconfig'.clangd.setup{
     on_attach = on_attach,
     flags = lsp_flags,
