733d035844bdf745785ad433a5ba53bdde3faf17
Enan Ajmain <3nan.ajmain@gmail.com>
2022-07-16 14:17:52 +0600

    vim: fixup lua funcs w/ nvim api

--- .config/nvim/lua/termutil/init.lua
+++ .config/nvim/lua/termutil/init.lua
@@ -2,24 +2,24 @@ local M = {}
 
 
 M.sendToTerm = function(visual)
-    assert(vim.g.termutilchan, "g:termutilchan not set")
+  assert(vim.g.termutilchan, "g:termutilchan not set")
 
-    local termutilchan = vim.g.termutilchan
+  local termutilchan = vim.g.termutilchan
 
-    if visual == 0 then
-        local line = vim.api.nvim_get_current_line()
+  if visual == 0 then
+    local line = vim.api.nvim_get_current_line()
+    vim.api.nvim_chan_send(termutilchan, line..'\n')
+  else
+    local lines = require("util.selection").get_selection()
+    for _, line in pairs(lines) do
+      if line ~= "" then
         vim.api.nvim_chan_send(termutilchan, line..'\n')
-    else
-        local lines = require("util.selection").get_selection()
-        for _, line in pairs(lines) do
-            if line ~= "" then
-                vim.api.nvim_chan_send(termutilchan, line..'\n')
-            end
-        end
-        if vim.bo.filetype == "python" then
-            vim.api.nvim_chan_send(termutilchan, '\n')
-        end
+      end
     end
+    if vim.bo.filetype == "python" then
+      vim.api.nvim_chan_send(termutilchan, '\n')
+    end
+  end
 end
 
 return M
--- .config/nvim/lua/util/selection.lua
+++ .config/nvim/lua/util/selection.lua
@@ -1,18 +1,18 @@
 local M = {}
 
 M.get_selection = function()
-    -- does not handle rectangular selection
-    local s_start = vim.fn.getpos("'<")
-    local s_end = vim.fn.getpos("'>")
-    local n_lines = math.abs(s_end[2] - s_start[2]) + 1
-    local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
-    lines[1] = string.sub(lines[1], s_start[3], -1)
-    if n_lines == 1 then
-        lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
-    else
-        lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
-    end
-    return lines
+  -- does not handle rectangular selection
+  local s_start = vim.api.nvim_buf_get_mark(0, "<")
+  local s_end = vim.api.nvim_buf_get_mark(0, ">")
+  local n_lines = math.abs(s_end[1] - s_start[1]) + 1
+  local lines = vim.api.nvim_buf_get_lines(0, s_start[1] - 1, s_end[1], false)
+  lines[1] = string.sub(lines[1], s_start[2], -1)
+  if n_lines == 1 then
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[2] - s_start[2] + 1)
+  else
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[2])
+  end
+  return lines
 end
 
 return M
