b670f075b49af043d667a36326b9bb61f8987a46
Enan Ajmain <3nan.ajmain@gmail.com>
2022-08-04 21:37:45 +0600

    Don't call vsdevcmd.bat in nvim build script
    Problem:
    The bat script was causing these errors:
        The input line is too long.
        The syntax of the command is incorrect.
    
    Solution:
    Use Launch-VsDevShell *before* running the build script. Then we don't
    need to run the bat script from the build script.

--- install_scripts/build_nvim.ps1
+++ install_scripts/build_nvim.ps1
@@ -14,9 +14,9 @@ $projectDir = [System.IO.Path]::GetFullPath("$(Get-Location)")
 $buildDir = Join-Path -Path $projectDir -ChildPath "build"
 
 Write-Host "############################################################"
-$DEPS_BUILD_DIR = [System.IO.Path]::GetFullPath($DepsDir)
+$env:DEPS_BUILD_DIR = [System.IO.Path]::GetFullPath($DepsDir)
 $buildDir = Join-Path -Path $projectDir -ChildPath "release"
-Write-Host "DEPS_BUILD_DIR" $DEPS_BUILD_DIR
+Write-Host "env:DEPS_BUILD_DIR" $env:DEPS_BUILD_DIR
 Write-Host "buildDir" $buildDir
 Write-Host "############################################################"
 
@@ -28,8 +28,8 @@ $nvimCmakeVars = @{
   CMAKE_BUILD_TYPE=$cmakeBuildType;
   BUSTED_OUTPUT_TYPE = 'nvim';
 }
-if ($null -eq $DEPS_BUILD_DIR) {
-  $DEPS_BUILD_DIR = Join-Path -Path $projectDir -ChildPath ".deps"
+if ($null -eq $env:DEPS_BUILD_DIR) {
+  $env:DEPS_BUILD_DIR = Join-Path -Path $projectDir -ChildPath ".deps"
 }
 
 function exitIfFailed() {
@@ -42,31 +42,22 @@ function convertToCmakeArgs($vars) {
   return $vars.GetEnumerator() | ForEach-Object { "-D$($_.Key)=$($_.Value)" }
 }
 
-$installationPath = C:\PROGRA~2\MICROS~2\Installer\vswhere.exe -latest -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
-if ($installationPath -and (Test-Path "$installationPath\Common7\Tools\vsdevcmd.bat")) {
-  & "${env:COMSPEC}" /s /c "`"$installationPath\Common7\Tools\vsdevcmd.bat`" -arch=x64 -no_logo && set" `
-    | ForEach-Object {
-    $name, $value = $_ -split '=', 2
-    Set-Content env:\"$name" $value
-  }
-}
-
 function BuildDeps {
-  if (Test-Path -PathType container $DEPS_BUILD_DIR) {
-    $cachedBuildTypeStr = $(Get-Content $DEPS_BUILD_DIR\CMakeCache.txt `
+  if (Test-Path -PathType container $env:DEPS_BUILD_DIR) {
+    $cachedBuildTypeStr = $(Get-Content $env:DEPS_BUILD_DIR\CMakeCache.txt `
                             | Select-String -Pattern "CMAKE_BUILD_TYPE.*=($cmakeBuildType)")
     if (-not $cachedBuildTypeStr) {
-      Write-Warning " unable to validate build type from cache dir $DEPS_BUILD_DIR"
+      Write-Warning " unable to validate build type from cache dir $env:DEPS_BUILD_DIR"
     }
   }
 
   # we currently can't use ninja for cmake.deps, see #19405
   $depsCmakeGenerator = "Visual Studio 16 2019"
   $depsCmakeGeneratorPlf = "x64"
-  cmake -S "$projectDir\cmake.deps" -B $DEPS_BUILD_DIR -G $depsCmakeGenerator -A $depsCmakeGeneratorPlf $(convertToCmakeArgs($depsCmakeVars)); exitIfFailed
+  cmake -S "$projectDir\cmake.deps" -B $env:DEPS_BUILD_DIR -G $depsCmakeGenerator -A $depsCmakeGeneratorPlf $(convertToCmakeArgs($depsCmakeVars)); exitIfFailed
 
   $depsCmakeNativeToolOptions= @('/verbosity:normal', '/m')
-  cmake --build $DEPS_BUILD_DIR --config $cmakeBuildType -- $depsCmakeNativeToolOptions; exitIfFailed
+  cmake --build $env:DEPS_BUILD_DIR --config $cmakeBuildType -- $depsCmakeNativeToolOptions; exitIfFailed
 }
 
 function Build {
