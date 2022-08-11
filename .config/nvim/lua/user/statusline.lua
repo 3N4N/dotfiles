08c159a228f5200f86223e417c77c3907a490ec4
Enan Ajmain <3nan.ajmain@gmail.com>
2022-08-11 12:41:59 +0600

    vim: revert 7a13bf2fe4fb

---
+++ .config/nvim/lua/user/statusline.lua
@@ -0,0 +1,47 @@
+-- Statusline ----------------------------------------------------------------
+
+vim.opt.laststatus = 2
+
+local PathShortenIfLong = function(path)
+  if strlen(path) > vim.fn.winwidth(0) - 10 then
+    return vim.fn.pathshorten(path)
+  end
+  return path
+end
+
+vim.opt.statusline = " %{&modified?'Δ':&readonly||!&modifiable?'ø':'✓'}"
+vim.opt.statusline:append(" %<%{expand('%:~:.')!=#''?v:lua.PathShortenIfLong(expand('%:~:.')):'[No Name]'}")
+vim.opt.statusline:append("%=")
+vim.opt.statusline:append(" %l: %v ")
+
+
+-- Tabline -------------------------------------------------------------------
+
+local MyTabLine = function()
+    local s = ""
+    for i = 1, vim.fn.tabpagenr('$') do
+        local tabnr = i
+        local winnr = vim.fn.tabpagewinnr(tabnr)
+        local buflist = vim.fn.tabpagebuflist(tabnr)
+        local bufnr = buflist[winnr]
+        local bufname = vim.fn.fnamemodify(vim.fn.bufname(bufnr), ':t')
+        s = s .. "%" .. tabnr .. "T"
+        if tabnr == vim.fn.tabpagenr() then
+            s = s .. "%#TabLineSel#"
+        else
+            s = s .. "%#TabLine#"
+        end
+        s = s .. " " .. tabnr
+        if bufname == "" then
+            s = s .. " [No Name] "
+        else
+            s = s .. " " .. bufname .. " "
+        end
+    end
+    s = s .. "%#TabLineFill#"
+    return s
+end
+
+vim.opt.showtabline = 1
+vim.opt.tabline = '%!v:lua.MyTabLine()'
+
