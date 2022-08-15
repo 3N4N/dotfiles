8930df1b2c05f4fea1035477893c36a50e1cfeb2
Enan Ajmain <3nan.ajmain@gmail.com>
2022-08-15 14:09:48 +0600

    cmd: update doskeys

--- profile.cmd
+++ profile.cmd
@@ -2,8 +2,8 @@
 
 PROMPT = $e]9;9;$p$e\$e[1;34m$p$e[1;0m$g$s
 
+:: aliases
 doskey o = start $*
-doskey cd = cd /d $*
 doskey ns = nslookup $*
 doskey ip = ipconfig $*
 doskey vi = nvim $*
@@ -13,12 +13,16 @@ doskey psgrep = tasklist ^| head -n 3 $T tasklist ^| grep --color -i $1
 doskey xclip = win32yank.exe $*
 doskey wts = nvim "%LOCALAPPDATA%/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/LocalState/settings.json"
 
+:: use directory stack
+doskey cd = pushd $*
+doskey pd = popd
+
+:: ffmpeg
 doskey ffmpeg = ffmpeg -hide_banner $*
 doskey ffplay = ffplay -hide_banner $*
 doskey ffprobe = ffprobe -hide_banner $*
 
-
-:: commands for for docker
+:: docker
 doskey dcbuild = docker-compose build
 doskey dcup = docker-compose up
 doskey dcdown = docker-compose down
@@ -31,7 +35,7 @@ doskey m64 = C:\msys64\msys2_shell.cmd -defterm -here -no-start -mingw64
 doskey m32 = C:\msys64\msys2_shell.cmd -defterm -here -no-start -mingw32
 doskey vsdev = C:\PROGRA~2\MICROS~2\2019\Community\Common7\Tools\VsDevCmd.bat -arch=x64 -host_arch=x64
 
-:: shortcuts to msys utilties
+:: msys tools
 doskey ls = "C:/msys64/usr/bin/ls" -NvhF --group-directories-first $*
 doskey l = "C:/msys64/usr/bin/ls" -NvhFl --group-directories-first --time-style=+ $*
 doskey la = "C:/msys64/usr/bin/ls" -NvhFl --group-directories-first --time-style=+ -A $*
@@ -45,8 +49,10 @@ doskey find = "C:/msys64/usr/bin/find.exe" $*
 doskey du = "C:/msys64/usr/bin/du.exe" $*
 doskey mpv = "C:/apps/mpv/mpv.exe" $*
 
+:: cmake
 doskey cmake = "c:/msys64/mingw64/bin/cmake.exe" $*
 doskey cmakec = "c:/msys64/mingw64/bin/cmake.exe" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 $*
 doskey cmaked = "c:/msys64/mingw64/bin/cmake.exe" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug $*
 
+:: prepend to PATH
 set PATH=c:\msys64\mingw64\bin;c:\msys64\usr\bin;%PATH%
