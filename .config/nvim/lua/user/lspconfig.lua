27e2c05c179b31f778ce3bb49103b5db04b7b0a1
Enan Ajmain <3nan.ajmain@gmail.com>
2022-10-30 22:05:57 +0600

    vim: add border around floating windows of LSP

--- .config/nvim/lua/user/lspconfig.lua
+++ .config/nvim/lua/user/lspconfig.lua
@@ -1,3 +1,26 @@
+local _border = "single"
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    border = _border
+  }
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+  vim.lsp.handlers.signature_help, {
+    border = _border
+  }
+)
+
+vim.diagnostic.config{
+  float = {border = _border},
+  severity_sort = true
+}
+
+require('lspconfig.ui.windows').default_options = {
+  border = _border
+}
+
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
