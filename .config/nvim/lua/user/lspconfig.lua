aaf268171da28658bda67b6d702ccecad90c9a3d
Enan Ajmain <3nan.ajmain@gmail.com>
2022-12-21 14:59:29 +0600

    vim/lsp: disable pyright type-checking

--- .config/nvim/lua/user/lspconfig.lua
+++ .config/nvim/lua/user/lspconfig.lua
@@ -65,8 +65,15 @@ if vim.g.enableLSP == 1 then
 end
 
 require'lspconfig'.pyright.setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
+  on_attach = on_attach,
+  flags = lsp_flags,
+  settings = {
+    ['python'] = {
+      analysis = {
+        typeCheckingMode = "off",
+      },
+    },
+  }
 }
 
 require'lspconfig'.rust_analyzer.setup{
