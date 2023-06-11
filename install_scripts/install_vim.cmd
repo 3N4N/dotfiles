04f7b73efc0bf6130ee2e80aa949a6ba38257803
Enan Ajmain <3nan.ajmain@gmail.com>
2023-06-11 13:03:24 +0600

    vim: support versioning in install script

--- install_scripts/install_vim.cmd
+++ install_scripts/install_vim.cmd
@@ -1,13 +1,12 @@
 @echo off
 
-rmdir /s /q vim
-mkdir vim
+set VIMVER=90
 
-xcopy /s runtime\* vim
-copy src\*.exe vim
-copy src\tee\tee.exe vim
-copy src\xxd\xxd.exe vim
+rmdir /s /q vim%VIMVER%
+mkdir vim%VIMVER%
 
-rmdir /s /q C:\apps\vim
-mkdir C:\apps\vim
-xcopy /s vim C:\apps\vim
+xcopy /s  runtime\*         vim%VIMVER%
+copy      src\vim.exe       vim%VIMVER%
+copy      src\vimrun.exe    vim%VIMVER%
+copy      src\tee\tee.exe   vim%VIMVER%
+copy      src\xxd\xxd.exe   vim%VIMVER%
