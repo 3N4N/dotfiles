e9a56600f33b9103538d4c987c530b1bd849ad0d
Enan Ajmain <3nan.ajmain@gmail.com>
2024-11-08 01:05:43 -0500

    cmd: update

--- cmd/profile.cmd
+++ cmd/profile.cmd
@@ -2,7 +2,7 @@
 
 PROMPT = $e]9;9;$p$e\$e[0;34m$p$e[1;0m$+$g$s
 
-doskey /macrofile=E:\projects\dotfiles\cmd\macinit
+doskey /macrofile=%USERPROFILE%\projects\dotfiles\cmd\macinit
 
 set CLINK_TERM_VE=\e[0 q\e[?12l
 set GCC_COLORS=error=01;31:warning=01;35:note=01;36:range1=32:range2=34:locus=35:quote=30:path=01;36:fixit-insert=32:fixit-delete=31:diff-filename=35:diff-hunk=32:diff-delete=31:diff-insert=32:type-diff=01;32:fnname=35;32:targs=35
@@ -11,4 +11,5 @@ set LESS=-iSMRF
 set VISUAL=vim
 
 REM prepend to PATH
-set PATH=%PATH%;%PROGRAMFILES%\Git\usr\bin;C:\mingw64\bin
+set PATH=%PATH%;%PROGRAMFILES%\Git\usr\bin;
+REM C:\mingw64\bin
