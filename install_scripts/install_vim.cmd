e91aa65dc08f30ab3b2b20090001b388e551ad64
Enan Ajmain <3nan.ajmain@gmail.com>
2025-01-08 02:50:41 -0500

    vim: installation script takes in VIM version

--- install_scripts/install_vim.cmd
+++ install_scripts/install_vim.cmd
@@ -1,12 +1,13 @@
 @echo off
 
-set VIMVER=90
+set VIMVER=%1
 
 rmdir /s /q vim%VIMVER%
 mkdir vim%VIMVER%
 
-xcopy /s  runtime\*         vim%VIMVER%
-copy      src\vim.exe       vim%VIMVER%
-copy      src\vimrun.exe    vim%VIMVER%
-copy      src\tee\tee.exe   vim%VIMVER%
-copy      src\xxd\xxd.exe   vim%VIMVER%
+xcopy /s    runtime           vim%VIMVER%\runtime\
+copy        src\vim.exe       vim%VIMVER%
+copy        src\gvim.exe      vim%VIMVER%
+copy        src\vimrun.exe    vim%VIMVER%
+copy        src\tee\tee.exe   vim%VIMVER%
+copy        src\xxd\xxd.exe   vim%VIMVER%
